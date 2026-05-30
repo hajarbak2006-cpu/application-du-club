950767fd93e44266e8ac47377be137613ec36725
Hajar Aalam <hajar.aalam-etu@etu.univh2c.ma>
2026-05-30 17:58:31 +0100

    Ajout du tableau des membres

--- src/components/members.jsx
+++ src/components/members.jsx
@@ -1,40 +1,25 @@
 import members from "../data/members";
-
 function Members() {
   return (
-    <section className="container mt-5">
-
+    <section id="members" className="container mt-5">
       <h2>Membres du bureau</h2>
 
       <div className="row">
-
         {members.map((member) => (
-
           <div
             key={member.id}
             className="col-md-3"
           >
-
             <div className="card shadow mb-3">
-
               <div className="card-body text-center">
-
                 <h5>{member.nom}</h5>
-
                 <p>{member.poste}</p>
-
               </div>
-
             </div>
-
           </div>
-
         ))}
-
       </div>
-
     </section>
   );
 }
-
 export default Members;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,9 @@
-import App from './App.jsx'
-createRoot(document.getElementById('root')).render(
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-)
+  </React.StrictMode>
+);
