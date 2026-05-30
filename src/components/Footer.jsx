2932dc0a37d76046b754f514690c19c2bc556dda
Hajar Aalam <hajar.aalam-etu@etu.univh2c.ma>
2026-05-30 20:19:19 +0100

    Ajout du tableau des membres

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 function Footer() {
-
   return (
     <footer className="bg-dark text-white text-center p-4 mt-5">
-
       <h5>Contact</h5>
-
       <p>Email :  <a href="mailto:aidevcommunityfsbm@gmail.com"> aidevcommunityfsbm@gmail.com</a></p>
       <p>Téléphone :<a href="tel:+212600000000"> +212 6 00 00 00 00</a></p>
       <p>Instagram : <a href="https://www.instagram.com/aidev_communityfsbm?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">aidevcommunityfsbm</a></p>
-
     </footer>
   );
 }
-
 export default Footer;
